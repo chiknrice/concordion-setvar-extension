520e3a755db1b49accd13ea9c9103c5903d2bff5
ian <chiknrice@gmail.com>
2015-09-06 02:18:09 +1200

    revert rename of SetEval - should keep the set - eval command strictly for <span> use

--- src/test/java/org/chiknrice/concordion/spec/SetEvalSpecFixture.java
+++ src/test/java/org/chiknrice/concordion/spec/SetEvalSpecFixture.java
@@ -3,5 +3,5 @@ package org.chiknrice.concordion.spec;
 /**
  * @author <a href="mailto:chiknrice@gmail.com">Ian Bondoc</a>
  */
-public class SetSpecFixture extends SetVarBaseFixture {
+public class SetEvalSpecFixture extends SetVarBaseFixture {
 }
