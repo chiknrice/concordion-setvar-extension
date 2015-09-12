7391715ffcd4349ab629f5ab63fc505ec4267b4b
ian <chiknrice@gmail.com>
2015-09-12 15:39:38 +1200

    only update concordion pages during release build

--- deploy_pages.sh
+++ deploy_pages.sh
@@ -10,12 +10,9 @@ cd gh-pages
 
 git clone --depth=50 --branch=gh-pages https://$GH_TOKEN@github.com/chiknrice/concordion-setvar-extension.git
 
-if [[ -n "${TRAVIS_TAG-}" ]]
-then
-    echo "Adding concordion specs for $TRAVIS_TAG"
-    mkdir concordion-setvar-extension/$TRAVIS_TAG
-    cp -R ../build/reports/spec/* concordion-setvar-extension/$TRAVIS_TAG
-fi
+echo "Adding concordion specs for $TRAVIS_TAG"
+mkdir concordion-setvar-extension/$TRAVIS_TAG
+cp -R ../build/reports/spec/* concordion-setvar-extension/$TRAVIS_TAG
 
 rm -rf concordion-setvar-extension/latest
 mkdir concordion-setvar-extension/latest
@@ -23,17 +20,14 @@ cp -R ../build/reports/spec/* concordion-setvar-extension/latest
 
 cd concordion-setvar-extension
 
-# currently always evaluates to true due to the timestamps in spec footers
-if [[ -n $(git status -s) ]]
-then
-    echo "Updating latest concordion specs"
+echo "Updating latest concordion specs"
 
-    git config user.email "chiknrice@gmail.com"
-    git config user.name "Travis CI"
-    git config push.default matching
+git config user.email "chiknrice@gmail.com"
+git config user.name "Travis CI"
+git config push.default matching
 
-    git add --all .
-    git commit -m "Update concordion spec result"
+git add --all .
+git commit -m "Update concordion spec result"
 
-    git push
-fi
+git push
+
